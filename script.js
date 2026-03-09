68bca6407558a5adb425a427e0a0fb9d01c80c5d
hasstepanian-wq <hasstepanian@gmail.com>
2026-03-09 13:36:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Живые часы
 setInterval(() => {
     const clock = document.getElementById('live-clock');
-    if (clock) {
-        clock.innerText = new Date().toLocaleTimeString('ru-RU');
-    }
+    if (clock) clock.innerText = new Date().toLocaleTimeString('ru-RU');
 }, 1000);
-
-// Логика перехода (пока просто консоль, чтобы не ломать ссылки)
-console.log("ЭТП ЭРА: Морда сайта загружена успешно.");
